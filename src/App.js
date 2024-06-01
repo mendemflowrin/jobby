7da9470916e6ab540523023b0204a66e833de0b4
mendemflowrin <nxtwave@ip-10-2-79-104.ap-south-1.compute.internal>
2024-06-01 04:39:32 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+
+import JobItemDetails from './components/JobItemDetails'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/Jobs" component={Jobs} />
+        <ProtectedRoute exact path="/Jobs/:id" component={JobItemDetails} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
+      </Switch>
+    )
+  }
+}
+
+export default App
+
+// These are the lists used in the application. You can move them to any component needed.
---
+++ src/components/SkillsCard/index.js
@@ -0,0 +1,13 @@
+const SkillsCard = props => {
+  const {skillsDetails} = props
+  const {imageUrl, name} = skillsDetails
+  return (
+    <li className="skills-item-container">
+      <div className="skills-container">
+        <img src={imageUrl} alt={name} className="skill-image" />
+        <p className="skill-name">{name}</p>
+      </div>
+    </li>
+  )
+}
+export default SkillsCard
